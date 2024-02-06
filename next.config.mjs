0fe5ab28bd0f898d5bbb77f410e71f6c33696e5a
Milad <milad.bhzz@gmail.com>
2024-02-07 01:43:13 +0330

    change setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/github-pages',
-};
-
 export default nextConfig;
+module.exports = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "": undefined,
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+    reactStrictMode: true,
+  }
